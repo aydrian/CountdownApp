af224393e344c41877fa2361b47f3ad1233a3921
Aydrian J. Howard <aydrian@gmail.com>
2017-02-16 14:49:38 -0500

    ListView with Row

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -2,59 +2,102 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  ActivityIndicator,
+  ListView,
+  AsyncStorage
 } from 'react-native';
+import Row from './Row';
 
 export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      loading: true,
+      items: [{key:1, date: '2017-07-25', description: 'My Birthday'}],
+      dataSource: ds.cloneWithRows([{key:1, date: '2017-07-25', description: 'My Birthday'}])
+    };
+    this.setSource = this.setSource.bind(this);
+  }
+
+  componentWillMount() {
+    AsyncStorage.getItem("countdownAppItems").then((json) => {
+      try {
+        const items = JSON.parse(json);
+        this.setSource(items, items, { loading: false});
+      } catch(e) {
+        this.setState({
+          loading: false
+        });
+      }
+    });
+  }
+
+  setSource(item, itemsDatasource, otherState = {}) {
+    this.setState({
+      items,
+      dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
+      ...otherState
+    });
+    //AsyncStorage.setItem("countdownAppItems", JSON.stringify(items));
+  }
+
   render() {
     return (
-      <View style={styles.mainContainer}>
-        <Text>Testing the Router</Text>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <ListView
+            style={styles.list}
+            enableEmptySections
+            dataSource={this.state.dataSource}
+            renderRow={({ key, ...value}) => {
+              return (
+                <Row
+                  key={key}
+                  {...value}
+                />
+              )
+            }}
+            renderSeparator={(sectionId, rowId) => {
+              return <View key={rowId} style={styles.separator} />
+            }}
+          />
+        </View>
+        {this.state.loading && <View style={styles.loading}>
+          <ActivityIndicator
+            animating
+            size="large"
+          />
+        </View>}
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
-        flex: 1,
-        padding: 30,
-        marginTop: 65,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        backgroundColor: '#48BBEC'
-    },
-    title: {
-        marginBottom: 20,
-        fontSize: 25,
-        textAlign: 'center',
-        color: '#fff'
-    },
-    searchInput: {
-        height: 50,
-        padding: 4,
-        marginRight: 5,
-        fontSize: 23,
-        borderWidth: 1,
-        borderColor: 'white',
-        borderRadius: 8,
-        color: 'white'
-    },
-    buttonText: {
-        fontSize: 18,
-        color: '#111',
-        alignSelf: 'center'
-    },
-    button: {
-        height: 45,
-        flexDirection: 'row',
-        backgroundColor: 'white',
-        borderColor: 'white',
-        borderWidth: 1,
-        borderRadius: 8,
-        marginBottom: 10,
-        marginTop: 10,
-        alignSelf: 'stretch',
-        justifyContent: 'center'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#F5F5F5'
+  },
+  loading: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,.2)"
+  },
+  content: {
+    flex: 1
+  },
+  list: {
+    backgroundColor: '#FFF'
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: "#F5F5F5"
+  }
 });
---
+++ App/Components/Row.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+import moment from 'moment';
+
+export default class Row extends Component {
+  render() {
+    const now = moment();
+    const date = moment(this.props.date);
+    const count = date.diff(now, 'days')
+    return (
+      <View style={styles.container}>
+        <View style={styles.categoryWrapper}>
+          <Text style={styles.category}>🕘</Text>
+        </View>
+        <View style={styles.textWrapper}>
+          <Text style={styles.description}>{this.props.description}</Text>
+          <Text style={styles.date}>{date.format('LL')}</Text>
+        </View>
+        <View style={styles.countWrapper}>
+          <Text style={styles.count}>{count}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  categoryWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  countWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  textWrapper: {
+    flex: 3,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  count: {
+    fontSize: 24,
+    color: '#0000ff'
+  },
+  category: {
+    fontSize: 24
+  },
+  date: {
+    fontSize:10,
+    color: '#4d4d4d'
+  },
+  description: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  }
+});
